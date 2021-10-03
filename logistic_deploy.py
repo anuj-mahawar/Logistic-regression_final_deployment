13dc069440d33f12f6605bad203d12eba4692cc9
Anuj Mahawar <anujmahawar2008@gmail.com>
2021-10-03 13:18:26 +0530

    make it better

--- logistic_deploy.py
+++ logistic_deploy.py
@@ -7,24 +7,25 @@ from flask import Flask, render_template
 class predObj:
 
     def predict_log(self, dict_pred):
-        with open("Final_standardScalar_Logistic_Prediction.sav", 'rb') as f:
+        with open("standardScalar_Logistic_Prediction.sav", 'rb') as f:
             #print('Inside Scalar')
             scalar = pickle.load(f)
 
-        with open("Final_model_For_Logistic_Prediction.sav", 'rb') as f:
+        with open("model_For_Logistic_Prediction.sav", 'rb') as f:
             #print('INside Model')
             model = pickle.load(f)
         data_df = pd.DataFrame(dict_pred,index=[1,])
-        #print(data_df)
+        #print("DF:",data_df)
+        #print(scalar,type(scalar))
         scaled_data = scalar.transform(data_df)
         predict = model.predict(scaled_data)
-        #print(predict)
+        print(predict)
         if predict[0] ==1 :
             result = 'Diabetic'
-            #print(result)
+            print(result)
         else:
             result ='Non-Diabetic'
-            #print(result)
+            print(result)
         return result
 
 
